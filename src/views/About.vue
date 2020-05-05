b9a4c34c34b909383ee44765577b78e0f1ebae0d
Tsukasa Sekiguchi <t14159265358979323846@gmail.com>
2020-05-05 16:01:58 +0900

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-app class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>About Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>This is about page</h1>
+      <router-link to="/">Got to home</router-link>
+    </ion-content>
+  </ion-app>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-app class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <hr />
+      <ion-button @click="onClick()" full>Go to About Page</ion-button>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </ion-content>
+  </ion-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    onClick() {
+      this.$router.push('about')
+    },
+  },
+})
+</script>
